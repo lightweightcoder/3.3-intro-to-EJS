ce6030ada38dd7020f6af97830eff08774c45137
aljt <alvinljt@gmail.com>
2020-11-12 21:24:39 +0800

    installed EJS syntax highlighter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+// "create" the server
+const app = express();
+
+// Port number
+const PORT = 3004;
+
+// set the library (template engine) to use for all requests
+app.set('view engine', 'ejs');
+
+// set the route
+app.get('/bananas', (request, response) => {
+  const data = {
+    user: {
+      name: 'kai',
+    },
+  };
+
+  // response.render take 2 arguments:
+  // 1. The path/name of a file (without extension) in the views directory.
+  // 2. Data that will be used to render the HTML. This is an object.
+  response.render('index', data);
+});
+
+// initialise the request listener port functionality
+app.listen(PORT);
